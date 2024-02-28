e8fcbe8b447a6873b90e7e8bc0b1a9e7e4859924
Michel <michelbreno5@hotmail.com>
2024-02-28 15:36:23 -0300

    feat(Config::Development): Adding sandbox api key

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,7 +82,7 @@ BoletoSimples.configure do |c|
   c.environment = :sandbox # defaut :sandbox
   # production - https://app.kobana.com.br/conta/api/tokens
   # sandbox - https://app-sandbox.kobana.com.br/conta/api/tokens
-  c.api_token = 'api-token'
+  c.api_token = 'gNbwHGWCA2CPe2PhnNjc5GwNrw_3CUGGnyH55ffvBwM'
   c.user_agent = 'michelbreno5@hotmail.com' #Colocar um e-mail válido para contatos técnicos relacionado ao uso da API.
   # c.debug = true
   # c.custom_headers = { 'X-CUSTOM' => 'CONTENT' }
