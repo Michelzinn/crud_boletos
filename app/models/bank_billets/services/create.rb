9eb6c0d93e8e3f9f02fe897db5324e8ff1ed73c7
Michel <michelbreno5@hotmail.com>
2024-02-29 10:21:26 -0300

    fix(BankBillets::Services::Create): Renaming variable

--- app/models/bank_billets/services/create.rb
+++ app/models/bank_billets/services/create.rb
@@ -1,7 +1,7 @@
 module BankBillets::Services
   class Create
-    def call(slip_service_handler, params)
-      bank_billet = slip_service_handler.create(params)
+    def call(billet_service_handler, params)
+      bank_billet = billet_service_handler.create(params)
 
       bank_billet.response_errors[:code]
     end
