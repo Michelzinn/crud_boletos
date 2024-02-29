a9d52e64df21ee69560dea7d4a44304cfe58a710
Michel <michelbreno5@hotmail.com>
2024-02-29 15:57:01 -0300

    feat(BankBilletsController): Setting up the billet creation via API and starting forms html

--- app/controllers/bank_billets_controller.rb
+++ app/controllers/bank_billets_controller.rb
@@ -4,4 +4,16 @@ class BankBilletsController < ActionController::Base
   def index
 
   end
+
+  def new
+    @states = States::OPTIONS
+  end
+
+  def create
+    params.permit!
+
+    service_status = BankBillets::Services::Create.new.call(BoletoSimples::BankBillet, params)
+
+    BankBillet.create(params) if service_status == '200'
+  end
 end
