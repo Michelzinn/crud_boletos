1b76d02c48ba1761189cd8e8aefba16d81d55ec6
Michel <michelbreno5@hotmail.com>
2024-03-01 19:21:30 -0300

    refactor(CreateSpec): Refactoring the test to make it better suit the new logic

--- spec/models/bank_billets/services/create_spec.rb
+++ spec/models/bank_billets/services/create_spec.rb
@@ -5,14 +5,13 @@ RSpec.describe BankBillets::Services::Create, type: :model do
     it '' do
       bank_billet_service_mock = instance_double('bank_billet_service_mock')
       bank_billet_mock = instance_double('bank_billet_mock')
-      expected_status_return = 'status returned by api'
 
       allow(bank_billet_service_mock).to receive(:create).and_return(bank_billet_mock)
-      allow(bank_billet_mock).to receive(:response_errors).and_return({ code: expected_status_return })
+      allow(bank_billet_mock).to receive(:response_errors).and_return({mock: 'mock'})
 
-      result = BankBillets::Services::Create.new.call(bank_billet_service_mock, {})
+      bank_billet_returned_from_service = BankBillets::Services::Create.new.call(bank_billet_service_mock, {})
 
-      expect(result).to be(expected_status_return)
+      expect(bank_billet_returned_from_service.response_errors).to eq({mock: 'mock'})
     end
   end
 end
